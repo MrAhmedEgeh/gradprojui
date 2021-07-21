f21d623b31db81edca5d9c73a9225bd7cd4f06ab
MrAhmedEgeh <mrxb114@gmail.com>
2021-07-21 23:37:05 +0300

    Registration implementation

--- Assets/Scenes/Register.cs
+++ Assets/Scenes/Register.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
-
+using System.Text.RegularExpressions;
 public class Register : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -23,15 +23,82 @@ public class Register : MonoBehaviour
         });
         registerBtn.onClick.AddListener(() =>
         {
-            if (string.IsNullOrEmpty(username.text) || string.IsNullOrEmpty(email.text) || string.IsNullOrEmpty(pass1.text) ||
-                string.IsNullOrEmpty(pass2.text))
+            if(allFieldsNotEmpty(username.text, email.text, pass1.text, pass2.text))
+            {
+                if (varifyEmail(email.text))
+                {
+                    if(varifyPassword(pass1.text, pass2.text))
+                    {
+                        //RegisterUser(username.text, email.text, pass1.text);
+                        notifications.text = "woooooooow";
+                    }
+                    else
+                    {
+                        notifications.text = "Passwords must match and be 6 digits";
+                    }
+                }
+                else
+                {
+                    notifications.text = "Please enter a valid email";
+                }
+            }
+            else
             {
                 notifications.text = "Please enter all fields";
             }
         });
     }
+    public bool allFieldsNotEmpty(string username, string email, string password1, string password2)
+    {
+        return string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password1) ||
+            string.IsNullOrEmpty(password2) ?  false : true;
+    }
+    public bool varifyEmail(string email)
+    {
+        Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        Match match = regex.Match(email);
+        return match.Success ? true : false;
+    }
+
+    public bool varifyPassword(string pass1, string pass2)
+    {
+        return pass1.Length >= 6 && pass2.Length >= 6 ? 
+               pass1.Equals(pass2) ? true : false 
+           : false;
+    }
     public void goToLogin()
     {
         SceneManager.LoadScene("LoginScene");
+    }
+
+    IEnumerator RegisterUser(string username, string emails, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("emails", emails);
+        form.AddField("password", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/gradProjectBackend/Register/Register.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+
+                if (www.downloadHandler.text.Equals("wrong credentials") || www.downloadHandler.text.Equals("Invalid data"))
+                {
+                    notifications.text = www.downloadHandler.text;
+                }
+                else
+                {
+                    SceneManager.LoadScene("LoginScene");
+                }
+
+            }
+        }
     }
 }
